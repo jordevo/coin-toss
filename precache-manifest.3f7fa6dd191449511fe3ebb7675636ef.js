d179b6bfb226fc4024039b751674caecc5390747
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2020-10-15 00:18:05 +0200

    Updates

---
+++ precache-manifest.3f7fa6dd191449511fe3ebb7675636ef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1014e8a56b4f7a66556631535b42847e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fac3512630d78107e78",
+    "url": "/static/css/main.a6b12dad.chunk.css"
+  },
+  {
+    "revision": "aaa8d155d6d6cd8b346b",
+    "url": "/static/js/2.ab3d0067.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ab3d0067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fac3512630d78107e78",
+    "url": "/static/js/main.9a254200.chunk.js"
+  },
+  {
+    "revision": "2f4a6d284ff948884433",
+    "url": "/static/js/runtime-main.60d69148.js"
+  },
+  {
+    "revision": "16faf88deef312469fb2c26867233dc7",
+    "url": "/static/media/heads.16faf88d.png"
+  },
+  {
+    "revision": "8a68d6021af321ac0fe48256088ad0b8",
+    "url": "/static/media/headsTrump.8a68d602.png"
+  },
+  {
+    "revision": "206883857c9bf4112e0b7fa109556f08",
+    "url": "/static/media/tails.20688385.png"
+  },
+  {
+    "revision": "6418a3c3edf4866c3f59a39452fc61a4",
+    "url": "/static/media/tailsTrump.6418a3c3.png"
+  }
+]);
---
+++ precache-manifest.bfa7bbe452c5d6f7837dcfa6d090e97f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c146ed0d7686719d928c022b2b8e18",
+    "url": "/coin-toss/index.html"
+  },
+  {
+    "revision": "1594889013147b51057d",
+    "url": "/coin-toss/static/css/main.2ec5916c.chunk.css"
+  },
+  {
+    "revision": "403e7e7f219a595a59b3",
+    "url": "/coin-toss/static/js/2.79954cd1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coin-toss/static/js/2.79954cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1594889013147b51057d",
+    "url": "/coin-toss/static/js/main.220fb0ba.chunk.js"
+  },
+  {
+    "revision": "76eb8607a4d03d36d820",
+    "url": "/coin-toss/static/js/runtime-main.462fd917.js"
+  },
+  {
+    "revision": "16faf88deef312469fb2c26867233dc7",
+    "url": "/coin-toss/static/media/heads.16faf88d.png"
+  },
+  {
+    "revision": "8a68d6021af321ac0fe48256088ad0b8",
+    "url": "/coin-toss/static/media/headsTrump.8a68d602.png"
+  },
+  {
+    "revision": "206883857c9bf4112e0b7fa109556f08",
+    "url": "/coin-toss/static/media/tails.20688385.png"
+  },
+  {
+    "revision": "6418a3c3edf4866c3f59a39452fc61a4",
+    "url": "/coin-toss/static/media/tailsTrump.6418a3c3.png"
+  }
+]);
