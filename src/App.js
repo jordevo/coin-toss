a28a39fa6e756ad3df6dc3e65b462ddabb4bc8a9
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2020-10-20 00:04:09 +0200

    use hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { Coin } from "./Coin";
 import "./App.css";
 
@@ -26,7 +26,7 @@ function App() {
           <Route exact path="/shapes">
             <Coin showShapes />
           </Route>
-          <Route path="/" component={() => <></>} />
+          <Route path="*" component={() => <></>} />
         </section>
       </div>
     </Router>
